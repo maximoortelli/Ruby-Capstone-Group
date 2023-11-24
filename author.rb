b2c6c06ba4dd205b19ec5e57093164f8c4eec21d
maximoortelli <maximoortelli@hotmail.com>
2023-11-24 12:34:42 -0300

    Update author and game.rb files
    Update author and game.rb files

--- author.rb
+++ author.rb
@@ -1,16 +1,24 @@
+require 'securerandom'
+
 class Author
   attr_accessor :first_name, :last_name
+  attr_reader :id
 
-  def initialize(id, first_name, last_name, items = [])
+  def initialize(first_name, last_name, items = [])
     @first_name = first_name
     @last_name = last_name
-    private
     @items = items
-    @id = id
+    @id = generate_id
   end
 
   def add_item(item)
     @items.push(item)
     item.author = self
+  end
+
+  private
+
+  def generate_id
+    SecureRandom.uuid
   end
 end
