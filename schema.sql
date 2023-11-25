31170f0000ec7656edea473ff09f87fa0bc26f4a
fmanimashaun <fisayo.animashaun@outlook.com>
2023-11-25 09:10:39 +0100

    Update schema

--- schema.sql
+++ schema.sql
@@ -1,53 +1,62 @@
-CREATE TABLE Label (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(255),
-    color VARCHAR(20)
+CREATE TABLE "label" (
+  "id" int PRIMARY KEY,
+  "title" varchar(255),
+  "color" varchar(255)
 );
 
-CREATE TABLE Genre (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255)
+CREATE TABLE "genre" (
+  "id" int PRIMARY KEY,
+  "name" varchar(255)
 );
 
-
-CREATE TABLE Item (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    genre_id INT,
-    author_id INT,
-    label_id INT,
-    publish_date DATE,
-    archived BOOLEAN,
-    FOREIGN KEY (genre_id) REFERENCES Genre(id),
-    FOREIGN KEY (author_id) REFERENCES Author(id),
-    FOREIGN KEY (label_id) REFERENCES Label(id)
+CREATE TABLE "author" (
+  "id" int PRIMARY KEY,
+  "first_name" varchar(255),
+  "last_name" varchar(255)
 );
 
-CREATE TABLE Book (
-    item_id INT PRIMARY KEY,
-    publisher VARCHAR(255),
-    cover_state VARCHAR(50),
-    FOREIGN KEY (item_id) REFERENCES Item(id)
+CREATE TABLE "source" (
+  "id" int PRIMARY KEY,
+  "name" varchar(255)
 );
 
-CREATE TABLE MusicAlbum (
-    item_id INT PRIMARY KEY,
-    on_spotify BOOLEAN,
+CREATE TABLE "item" (
+  "id" int PRIMARY KEY,
+  "author_id" int,
+  "genre_id" int,
+  "source_id" int,
+  "label_id" int,
+  "publish_date" date,
+  "archived" boolean,
+  
+  FOREIGN KEY ("author_id") REFERENCES "author" ("id"),
+  FOREIGN KEY ("genre_id") REFERENCES "genre" ("id"),
+  FOREIGN KEY ("source_id") REFERENCES "source" ("id"),
+  FOREIGN KEY ("label_id") REFERENCES "label" ("id")
 );
 
-
-CREATE TABLE Games (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    multiplayer BOOLEAN NOT NULL,
-    last_payed_at DATE NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (item_id) REFERENCES Item(id)
+CREATE TABLE "book" (
+  "id" int PRIMARY KEY,
+  "publisher" varchar(255),
+  "cover_state" varchar(255),
+  FOREIGN KEY ("id") REFERENCES "item" ("id")
 );
 
+CREATE TABLE "music_album" (
+  "id" int PRIMARY KEY,
+  "on_spotify" boolean,
+  FOREIGN KEY ("id") REFERENCES "item" ("id")
+);
 
-CREATE TABLE Author (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    items INTEGER,
-    PRIMARY KEY (id)
+CREATE TABLE "movie" (
+  "id" int PRIMARY KEY,
+  "silent" boolean,
+  FOREIGN KEY ("id") REFERENCES "item" ("id")
+);
+
+CREATE TABLE "game" (
+  "id" int PRIMARY KEY,
+  "multiplayer" boolean,
+  "last_played_at" date,
+  FOREIGN KEY ("id") REFERENCES "item" ("id")
 );
